4352639523fe935eca9a3cbbdbb5f0a676eb7b19
max <fantomax0@gmail.com>
2016-12-18 15:22:41 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# EnhydraLutris (FrontEnd Tofee)
+
+ALL scripts must be in <code>index.html</code>
+
+DON'T write anything else to <code>index.html</code>
+
+To change url:
+1. In template: <code>\<a ui-sref="url_name">Name\</a></code>
+2. In controller: <code>$state.go('url_name')</code>
+3. Check <code>config.js</code> to understand url dispatching
+
+Project structure:<br>
+<code>src/moduleName/(css/js/templates)</code><br>
+<code>js/(controllers/services/etc/config.js)</code>
---
+++ src/main/js/config.js
@@ -0,0 +1,26 @@
+(function(){
+    angular.module('tofi', ['ui.router']);
+
+    angular
+        .module('tofi')
+        .config(config)
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/login");
+        $stateProvider
+            .state("login", {
+                url: "/login",
+                // controller: 'loginCtrl',
+                // controllerAs: 'ctrl',
+                templateUrl: "src/main/templates/login.html"
+            })
+            .state("signup", {
+                url: "/signup",
+                // controller: 'registerCtrl',
+                // controllerAs: 'ctrl',
+                templateUrl: "src/main/templates/signup.html"
+            });
+    }
+})();
+
